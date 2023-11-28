a32d731c767555063642a9a14018bf7231c11bf0
Arthur Scharfenberger <64555332+ArthurScharfenberger@users.noreply.github.com>
2023-11-28 15:40:48 -0300

    FullStack Academy #3

--- admin.php
+++ admin.php
@@ -3,6 +3,7 @@ include_once 'config/config.php';
 include_once 'classes/Crud.php';
 $crud = new Crud($db);
 $data = $crud->read();
+
 ?>
 
 <!DOCTYPE html>
@@ -24,15 +25,6 @@ $data = $crud->read();
                 <img src="img/logo.png">
             </a>
         </div>
-
-        <ul class="nav-links">
-            <li class="link"><a href="#">Início</a></li>
-            <li id="link1" class="link"><a href="#">Características</a></li>
-            <li id="link2" class="link"><a href="#">Cursos</a></li>
-            <li id="link3" class="link"><a href="#">Sobre</a></li>
-        </ul>
-        <a href='./Login.php'> <button class="btn">Acessar</button></a>
-
     </nav>
 
     <header class="container">
@@ -43,19 +35,6 @@ $data = $crud->read();
             <H4>Usuários cadastrados</H4>
         </div>
     </header>
-
-    <section>
-        <div class="table">
-            <table>
-                <tr>
-                    <td>
-                        <h1>Usuário</h1>
-                        <h1>Curso cadastrado:</h1>
-                    </td>
-                </tr>
-            </table>
-        </div>
-    </section>
 
     <div class="copyright">
         Copyright © 2023 FullStack Academy. Todos os direitos reservados.
